dce153d2da3ebaad62d566fb42d9a3bad976c27f
Gus <august.colonna@gmail.com>
2023-05-23 17:43:21 +0200

    some styling done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
 import { Link } from "react-router-dom";
 
+import { SessionContext } from "../contexts/SessionContext";
+import { useContext } from "react";
+
 function NavBar() {
+
+const {token} = useContext(SessionContext)
+
   return (
-    <div className="navBar">
+    <div>
+      <div className="space-top"></div>
+      <div className="navBar wavy">
       <div className="logo">
         <Link to="/"><span>Why</span> Do You Know that?</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
-        <li>
+      {!token &&<li>
+         <Link to="/signup">Sign Up</Link>
+        </li>}
+        {!token &&<li>
           <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
-        <li>
+        </li>}
+        {token && <li>
+          <Link to="/profile" >Profile</Link>
+        </li>}
+        {token &&<li>
           <Link to="/decklist">Decks</Link>
-        </li>
-        <li>
+        </li>}
+        {token &&<li>
           <Link to="/createdeck">Create a New Deck</Link>
-        </li>
+        </li>}
         <li>
           <Link to="/game">Play</Link>
         </li>
       </ul>
+    </div>
     </div>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,24 +1,29 @@
 import NavBar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
     <div>
-      <div>
-        <NavBar />
-      </div>
-      <div className="preview-container">
-        <div className="hidden-card">
-          <p>Here?</p>
+      <NavBar />
+      <div className="landing-page-section">
+        <div className="directions">
+          <p>
+            Would you say you know a lot of facts, that maybe you shouldnt? Or,
+            would you consider youself as someone who is just really good at
+            guessing? Either way, this game is for you! There will be 3 cards
+            placed in front of you - the task is to guess which order they
+            should be in, based off of the scores, images, and titles. Make
+            sense? Give it a try!
+          </p>
         </div>
-        <div className="card-preview one"></div>
-        <div className="hidden-card">
-          <p>Here?</p>
+        <div className="preview-container">
+          <div className="card-preview one"></div>
+          <div className="card-preview two"></div>
+          <div className="card-preview three"></div>
         </div>
-        <div className="card-preview two"></div>
-        <div className="card-preview three"></div>
-        <div className="hidden-card">
-          <p>Here?</p>
-        </div>
+        <Link className="btn" to={"/game"}>
+          Play!
+        </Link>
       </div>
     </div>
   );
