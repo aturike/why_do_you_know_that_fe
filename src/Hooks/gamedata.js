6fd01181924d8463d2b0dade6cadf45f2c647e0e
Adam Turi <aturike@gmail.com>
2023-06-06 11:49:41 +0200

    done

--- src/Hooks/gamedata.js
+++ src/Hooks/gamedata.js
@@ -21,16 +21,24 @@ const gameCardsSelect = (nrDisplayCards, allDecks, score) => {
   const gameCards = indexArr
     .filter((indexElement) => indexElement !== targetIndex)
     .map((element) => allDecks[score].cards[element])
-    .sort((a, b) => a.value - b.value);
+    .sort((a, b) => a.value - b.value)
+    .slice(0, nrDisplayCards - 1);
 
   gameCards.push(targetCard);
 
   for (let i = 1; i <= nrDisplayCards * 2; i++) {
     if (i % 2 === 0) {
       const index = i / 2 - 1;
-      gameCardsDiv.push(gameCards[index]);
+      if (index === gameCards.length - 1) {
+        gameCardsDiv.push({ ...gameCards[index], target: true });
+      } else {
+        gameCardsDiv.push({ ...gameCards[index], target: false });
+      }
     } else {
-      gameCardsDiv.push({ _id: Math.round(Math.random() * 1000) });
+      gameCardsDiv.push({
+        _id: Math.round(Math.random() * 1000),
+        target: false,
+      });
     }
   }
 
